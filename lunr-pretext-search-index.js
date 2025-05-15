21c33d3543561309335b00394267e2bc875a8a13
bonniecjacob <bcjntm@rit.edu>
2025-05-15 22:54:13 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,13 +10,121 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
+  "id": "sec-our-first-day",
+  "level": "1",
+  "url": "sec-our-first-day.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Our First Day",
+  "body": " Our First Day  Here is the beginning of our section!  I just created a paragraph. Let's include some emphasized text. This is emphasized text. I need a quote too. Here. I need a term too. A term is something I define. And also some math, because why not. and .  These are my favorite animals, ordered in how I thought of them.   koalas   who doesn't like chickens?   Chickens    Pigeons     Now I'll tell you my favorite numbers in no particular order.   7    32    22      Hi!       1 2 3    4 5 6     Now with LaTeX...                             S_0    S_1   0  0  1  1       Do loops work?   And here is the one that didn't work:      graph={1:[3,4,5,5],2:[4,5],3:[4,5],6:[2,4]}          "
+},
+{
+  "id": "sec-our-first-day-3",
+  "level": "2",
+  "url": "sec-our-first-day.html#sec-our-first-day-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "term "
+},
+{
+  "id": "sec-our-first-day-9",
+  "level": "2",
+  "url": "sec-our-first-day.html#sec-our-first-day-9",
+  "type": "Figure",
+  "number": "1.1.1",
+  "title": "",
+  "body": "                  "
+},
+{
+  "id": "sec-our-first-day-10",
+  "level": "2",
+  "url": "sec-our-first-day.html#sec-our-first-day-10",
+  "type": "Figure",
+  "number": "1.1.2",
+  "title": "",
+  "body": "       S_0    S_1   0  0  1  1       Do loops work?  "
+},
+{
+  "id": "sec-our-first-day-12",
+  "level": "2",
+  "url": "sec-our-first-day.html#sec-our-first-day-12",
+  "type": "Figure",
+  "number": "1.1.3",
+  "title": "",
+  "body": "    graph={1:[3,4,5,5],2:[4,5],3:[4,5],6:[2,4]}         "
+},
+{
+  "id": "sec-more-stuff",
+  "level": "1",
+  "url": "sec-more-stuff.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "More stuff",
+  "body": " More stuff  Just trying to create a new section!    A graph is a triple such that ....      A graph is a triple such that ....    "
+},
+{
+  "id": "def-graph",
+  "level": "2",
+  "url": "sec-more-stuff.html#def-graph",
+  "type": "Definition",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  A graph is a triple such that ....   "
+},
+{
+  "id": "axiom-graph",
+  "level": "2",
+  "url": "sec-more-stuff.html#axiom-graph",
+  "type": "Axiom",
+  "number": "1.2.2",
+  "title": "",
+  "body": "  A graph is a triple such that ....   "
+},
+{
   "id": "sec-section-name",
   "level": "1",
   "url": "sec-section-name.html",
   "type": "Section",
-  "number": "1.1",
+  "number": "1.3",
   "title": "Section Title",
-  "body": " Section Title  Text of section.  Here is my contribution.  "
+  "body": " Section Title  Text of section.   Here is my contribution.  "
+},
+{
+  "id": "sec-section-trying-tex-to-pretext",
+  "level": "1",
+  "url": "sec-section-trying-tex-to-pretext.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "LaTeX converting to PreTeXt",
+  "body": " LaTeX converting to PreTeXt    The quantity is more than . And if you want to know, .     Well, I said so.   "
+},
+{
+  "id": "sec-section-trying-tex-to-pretext-2",
+  "level": "2",
+  "url": "sec-section-trying-tex-to-pretext.html#sec-section-trying-tex-to-pretext-2",
+  "type": "Theorem",
+  "number": "1.4.1",
+  "title": "",
+  "body": "  The quantity is more than . And if you want to know, .   "
+},
+{
+  "id": "sec-section-trying-tex-to-pretext-3",
+  "level": "2",
+  "url": "sec-section-trying-tex-to-pretext.html#sec-section-trying-tex-to-pretext-3",
+  "type": "Proof",
+  "number": "1.4.1",
+  "title": "",
+  "body": " Well, I said so.  "
+},
+{
+  "id": "sec-section-trying-more-tex",
+  "level": "1",
+  "url": "sec-section-trying-more-tex.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "LaTeX converting to PreTeXt again",
+  "body": " LaTeX converting to PreTeXt again   Hello.      "
 },
 {
   "id": "backmatter-2",
